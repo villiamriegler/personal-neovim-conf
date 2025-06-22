168530c45a84fa9f7300edc4c92fed59164dcf31
Villiam riegler <ville@riegler.se>
2025-06-22 16:10:41 +0200

    Ok getting the hang of it

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -16,7 +16,7 @@ return {
 				virtual_text = {
 					spacing = 4,
 					source = "if_many",
-					prefix = "●",
+					prefix = "icons",
 					-- this will set set the prefix to a function that returns the diagnostics icon based on the severity
 					-- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
 					-- prefix = "icons",
